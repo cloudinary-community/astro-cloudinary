a85aab72e8709d305981b69c8ac006e0c26623e6
Colby Fayock <fayock@gmail.com>
2024-08-07 16:55:14 -0400

    feat: fixing included props, updating docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,35 +1,43 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Astro Cloudinary',
-      social: {
-        github: 'https://github.com/colbyfayock/astro-cloudinary'
-      },
-      sidebar: [{
-        label: 'CldImage',
-        items: [{
-          label: 'Basic Usage',
-          slug: 'cldimage/basic-usage'
-        }]
+  integrations: [starlight({
+    title: 'Astro Cloudinary',
+    social: {
+      github: 'https://github.com/colbyfayock/astro-cloudinary'
+    },
+    sidebar: [{
+      label: 'CldImage',
+      items: [{
+        label: 'Basic Usage',
+        slug: 'cldimage/basic-usage'
       }, {
-        label: 'CldVideoPlayer',
-        items: [{
-          label: 'Basic Usage',
-          slug: 'cldvideoplayer/basic-usage'
-        }]
-      }],
-      customCss: [
-        './src/styles/tailwind.css',
-      ],
-    }), 
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ]
+        label: 'Configuration',
+        slug: 'cldimage/configuration'
+      }, {
+        label: 'Examples',
+        slug: 'cldimage/examples'
+      }]
+    }, {
+      label: 'CldVideoPlayer',
+      items: [{
+        label: 'Basic Usage',
+        slug: 'cldvideoplayer/basic-usage'
+      }]
+    }, {
+      label: 'Guides',
+      items: [{
+        label: 'Responzive Sizing',
+        slug: 'guides/responsive-sizing'
+      }]
+    }],
+    customCss: ['./src/styles/tailwind.css']
+  }), tailwind({
+    applyBaseStyles: false
+  }), icon()]
 });
--- docs/src/lib/util.ts
+++ docs/src/lib/util.ts
@@ -1,6 +1,4 @@
-// @ts-expect-error
 import { clsx } from 'clsx';
-// @ts-expect-error
 import { twMerge } from 'tailwind-merge';
 
 // @ts-expect-error
