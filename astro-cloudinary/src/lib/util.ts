eb3a8ff6529a0e17d4138c2a7fc3ae9c249bc1d7
Colby Fayock <colbyfayock@users.noreply.github.com>
2024-10-01 13:28:08 -0400

    feat: Fixes JS Render Blocking (#34)
    * loading js script dynamically to avoid render blocking behavior
    
    * adding checks for widget/player instances before trying to load the script, adding dev mode warnings

--- astro-cloudinary/src/lib/util.ts
+++ astro-cloudinary/src/lib/util.ts
@@ -9,3 +9,44 @@ export function triggerOnIdle(callback: any) {
   }
   return setTimeout(() => callback(), 1);
 }
+
+/**
+ * loadScript
+ */
+
+export function loadScript(src: string) {
+  return new Promise((resolve, reject) => {
+    if ( typeof document === 'undefined' ) {
+      reject('Can not be loaded on')
+    }
+
+    const script = document.createElement('script');
+
+    script.src = src;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    function handleOnLoad() {
+      cleanup();
+      resolve({
+        script
+      });
+    }
+
+    function handleOnError() {
+      cleanup();
+      reject({
+        script
+      })
+    }
+
+    script.addEventListener('load', handleOnLoad);
+    script.addEventListener('error', handleOnError);
+
+    function cleanup() {
+      script.removeEventListener('load', handleOnLoad);
+      script.removeEventListener('error', handleOnError);
+    }
+  })
+}
